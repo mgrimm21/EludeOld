8c7003a5024df78c2fd18c56cc63e20eb9485614
Mgrimm21 <mgrimm0021@live.com>
2018-03-29 15:06:24 -0400

    test

---
+++ src/com/mgrimm21/elude/Launcher.java
@@ -0,0 +1,12 @@
+package com.mgrimm21.elude;
+
+import java.awt.Toolkit;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		Toolkit tk = Toolkit.getDefaultToolkit();
+		tk.beep();
+	}
+
+}
